ca1d789790c4f18f23ae4d59d3542569e7b9dc61
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-04-28 00:10:26 +0200

    Add rxi.display.

--- src/rxwidgets/ipython/__init__.py
+++ src/rxwidgets/ipython/__init__.py
@@ -2,7 +2,7 @@ from .defer import defer, Defer, DeferError
 from .flatmap import flatten
 from .interact import interact, interact_manual
 from .load import long, LoadingError
-from .print import print_stream as print
+from .display import print_stream as print, display_stream as display
 from .stream import stream_defaults, stream_binding, apply, ConsequentialError
 from .voodoo import voodoo
 from . import parameters, widgets
--- src/rxwidgets/ipython/display.py
+++ src/rxwidgets/ipython/display.py
@@ -1,6 +1,7 @@
 import reactivex as rx
 
 from rxwidgets.ipython.stream import stream_defaults, apply
+from IPython.display import display
 
 
 def print_stream(observable: rx.Observable) -> rx.Observable:
@@ -11,5 +12,17 @@ def print_stream(observable: rx.Observable) -> rx.Observable:
     @stream_defaults
     def run(x=observable):
         print(x)
+
+    return run
+
+
+def display_stream(observable: rx.Observable) -> rx.Observable:
+    """
+    Convenient way to print / display the contents of a stream.
+    """
+    @apply
+    @stream_defaults
+    def run(x=observable):
+        display(x)
 
     return run
