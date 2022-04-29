b133314d5ddf4d654555f50be2f6057cbac2b8ad
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-04-29 14:43:16 +0200

    Show hidden rows with a special additional row to select the rest at once.

--- src/rxwidgets/ipython/pandas/dataframe/select_row.py
+++ src/rxwidgets/ipython/pandas/dataframe/select_row.py
@@ -9,6 +9,9 @@ from rxwidgets import valuebox
 from rxwidgets.ipython import DeferError
 
 
+_hidden_rows = object()
+
+
 def _row_to_label(row):
     def format_value(value):
         if isinstance(value, Enum):
@@ -23,7 +26,7 @@ def select_row(
     description="",
     multi=False,
     rows=15,
-    max_rows=1000,
+    max_rows=500,
     shown_columns=None,
     row_to_label=_row_to_label,
 ):
@@ -39,17 +42,21 @@ def select_row(
         ))
         ```
     """
-
-    if max_rows is not None:
-        dataframe = dataframe.iloc[:max_rows]
+    hidden_rows = None
+    shown_rows = dataframe.index
+    if max_rows is not None and len(dataframe) > max_rows + 1:
+        hidden_rows = dataframe.index[max_rows:]
+        shown_rows = dataframe.index[:max_rows]
 
     if shown_columns is None:
         shown_columns = dataframe.columns
 
     options = [
         (row_to_label(row), index)
-        for index, row in dataframe[shown_columns].iterrows()
+        for index, row in dataframe[shown_columns].loc[shown_rows].iterrows()
     ]
+    if hidden_rows is not None:
+        options.append((f"... {len(hidden_rows)} more", _hidden_rows))
 
     Component = ipywidgets.SelectMultiple if multi else ipywidgets.Select
     select: ipywidgets.widgets.widget_selection._Selection = Component(
@@ -62,12 +69,17 @@ def select_row(
     subject = rx.subject.ReplaySubject(1)
 
     def on_select(selected):
-        if multi:
-            selected = [s for s in selected if s is not None]
-
         if selected is None:
             subject.on_next(valuebox.ValueBox(DeferError(), is_error=True))
             return
+
+        if multi:
+            if selected[-1] is _hidden_rows:
+                selected = [*selected[:-1], *selected[-1]]
+        else:
+            if selected is _hidden_rows:
+                # Can't select a multi row in a single selection widget.
+                subject.on_next(valuebox.ValueBox(DeferError(), is_error=True))
 
         subject.on_next(valuebox.ValueBox(dataframe.loc[selected, :]))
 
