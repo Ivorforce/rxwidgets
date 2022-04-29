7ff78cacdac7d997ba2ddf271b15e34fac2053a7
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-04-29 11:56:00 +0200

    Abstact the dataframe creation of paths through a scheme.

--- src/rxwidgets/ipython/pandas/__init__.py
+++ src/rxwidgets/ipython/pandas/__init__.py
@@ -1,2 +1,2 @@
 from . import dataframe, paths
-from .paths import select
+from .paths import select, paths_to_dataframe
--- src/rxwidgets/ipython/pandas/paths.py
+++ src/rxwidgets/ipython/pandas/paths.py
@@ -1,4 +1,5 @@
 from typing import Sequence
+import reactivex as rx
 
 import ipywidgets
 from pathlib import Path
@@ -7,19 +8,30 @@ import pandas as pd
 from IPython.core.display_functions import display
 
 
-def path_to_dict(path, scheme_parts):
+def paths_to_dataframe(paths: Sequence, *, scheme=None) -> pd.DataFrame:
     """
-    Convert a path to a dictionary using a path scheme. The path scheme
-    is also a Path-like, describing the parts of the path. Underscores are ignored.
+    Convert a sequence of paths to a dataframe, interpreting path parts using a scheme.
+    If scheme is None, the dataframe will only have the column 'path'.
+    Underscores (_) are ignored.
     """
-    return {
-        "path": path,
-        **{
-            part_name: part
-            for part_name, part in zip(scheme_parts, path.parts)
-            if part_name != "_"
+    scheme_parts = Path(scheme).parts if scheme is not None else []
+    path_parts = [name for name in scheme_parts if name != "_"]
+
+    def path_to_dict(path):
+        return {
+            "path": path,
+            **{
+                part_name: part
+                for part_name, part in zip(scheme_parts, path.parts)
+                if part_name != "_"
+            }
         }
-    }
+
+    return pd.DataFrame([
+        path_to_dict(path)
+        for path in map(Path, paths)
+        if len(path.parts) >= len(scheme_parts)
+    ], columns=['path', *path_parts])
 
 
 def select(
@@ -27,18 +39,15 @@ def select(
     scheme=None,
     *,
     multi=False,
-    require_full_scheme=True
-):
+) -> rx.Observable:
     """
-    Select file paths.
+    Select file paths with pandas.dataframe.filter and pandas.dataframe.select_row.
 
     Args:
         paths: Sequence of path-likes.
         scheme: Naming scheme for attributes based from the path. Underscores (_) are
             ignored.
         multi: False to yield single rows, True to yield sub-selected dataframes.
-        require_full_scheme: Ignore files that are nested in a directory not fulfilling
-            to the full scheme.
 
     Examples:
         # Files in a folders structure like `~/my-files/team1/project1/file1.txt`.
@@ -54,15 +63,7 @@ def select(
     """
     import rxwidgets.ipython as rxi
 
-    scheme_parts = Path(scheme).parts if scheme is not None else []
-    required_path_len = len(scheme_parts) if require_full_scheme else 0
-    path_parts = [name for name in scheme_parts if name != "_"]
-
-    df = pd.DataFrame([
-        path_to_dict(path, scheme_parts=scheme_parts)
-        for path in map(Path, paths)
-        if len(path.parts) >= required_path_len
-    ], columns=['path', *path_parts])
+    df = paths_to_dataframe(paths, scheme=scheme)
 
     left_output = ipywidgets.Output()
     right_screen = rxi.Screen()
@@ -74,7 +75,8 @@ def select(
     with left_output:
         df_stream = rxi.pandas.dataframe.filter(
             df,
-            columns=path_parts,
+            # First column is the full path
+            columns=df.columns[1:],
             search_columns=['path'],
             search_index=False
         )
